fe55fcb1d786e9f612286caa5f971cd1f17e8fc1
Pruvost-Lison <lison.pruvost@edu.univ-fcomte.fr>
2025-05-14 14:20:42 +0200

    ajout du backend recettes

---
+++ backend/testback.js
@@ -0,0 +1,51 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('https://pb-yumly.leo-baudry.fr');
+
+// Récupérer toutes les recettes
+export async function getAllRecettes() {
+    return await pb.collection('Recette').getFullList({
+        sort: 'nom_recette'
+    });
+}
+
+// Récupérer une recette par ID
+export async function getRecetteById(id) {
+    return await pb.collection('Recette').getOne(id);
+}
+
+// Récupérer tous les noms de recettes
+export async function getAllRecetteNames() {
+    const recettes = await pb.collection('Recette').getFullList({
+        fields: 'id,nom_recette'
+    });
+    return recettes;
+}
+
+// Récupérer toutes les descriptions de recettes
+export async function getAllRecetteDescriptions() {
+    const recettes = await pb.collection('Recette').getFullList({
+        fields: 'id,description_recette'
+    });
+    return recettes;
+}
+
+// Ajouter ou modifier une recette
+export async function updateRecette(id, data) {
+    await pb.collection('Recette').update(id, data);
+}
+
+// Créer une nouvelle recette
+export async function createRecette(data) {
+    return await pb.collection('Recette').create(data);
+}
+
+export async function getRecettesByContinent(continent) {
+    if (!continent || continent === "Tous") {
+        return await getAllRecettes();
+    }
+
+    return await pb.collection('Recette').getFullList({
+        filter: `continent = "${continent}"`,
+        sort: 'nom_recette'
+    });
+}
